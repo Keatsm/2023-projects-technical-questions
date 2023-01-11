39aa8b91e9c790a7ca946fe8cba95c8cecf7df45
Keatsm <keatsm@zoho.com>
2023-01-12 00:42:17 +1100

    Done Backend challenge

--- q2-backend/ts_template/roundupper_100.ts
+++ q2-backend/ts_template/roundupper_100.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import morgan from 'morgan';
 
 // location is the simple (x, y) coordinates of an entity within the system
 // spaceCowboy models a cowboy in our super amazing system
@@ -15,18 +16,69 @@ type spaceEntity =
 
 // === ADD YOUR CODE BELOW :D ===
 
+type spaceAnimalCondensed = { type: "pig" | "cow" | "flying_burger", location: location };
+type spaceEntityCowboy = { type: "space_cowboy", metadata: spaceCowboy, location: location };
+
+// Would have put these functions in another file but wasn't sure
+// if you wanted all of it in this file
+
+const addEntity = (newEntities: spaceEntity[], spaceDatabase: spaceEntity[]) => {
+    for (const entity of newEntities) {
+        if (entity.type !== "space_cowboy" &&
+            entity.type !== "space_animal") continue;
+        spaceDatabase.push(entity);
+    }
+}
+
+const getAnimals = (cowboyName: string, spaceDatabase: spaceEntity[]) => {
+    const animalsWithinDistance = [] as spaceAnimalCondensed[];
+    const cowboy = spaceDatabase.find(entity => {
+        if (entity.type === 'space_cowboy') {
+            if (entity.metadata.name === cowboyName) return true;
+        }
+    });
+    if (cowboy === undefined || cowboy.type === 'space_animal') return animalsWithinDistance;
+    for (const entity of spaceDatabase) {
+        if (entity.type !== 'space_animal') continue;
+        if (withinDistance(entity, cowboy)) {
+            animalsWithinDistance.push({
+                type: entity.metadata.type,
+                location: entity.location
+            })
+        }
+    }
+    return animalsWithinDistance;
+}
+
+const withinDistance = (spaceAnimal: spaceEntity, cowboy: spaceEntityCowboy) => {
+    if (Math.abs(pythagorean(spaceAnimal.location.x, spaceAnimal.location.y)  -
+                 pythagorean(cowboy.location.x, cowboy.location.y))
+    <= cowboy.metadata.lassoLength) return true;
+    return false;
+}
+
+const pythagorean = (sideA: number, sideB: number) => {
+    return Math.sqrt(Math.pow(sideA, 2) + Math.pow(sideB, 2));
+  }
+
 // === ExpressJS setup + Server setup ===
 const spaceDatabase = [] as spaceEntity[];
 const app = express();
+// parse body
+app.use(express.json());
 
 // the POST /entity endpoint adds an entity to your global space database
 app.post('/entity', (req, res) => {
-    // TODO: fill me in
+    addEntity(req.body.entities as spaceEntity[], spaceDatabase);
+    res.json();
 });
 
 // lasooable returns all the space animals a space cowboy can lasso given their name
 app.get('/lassoable', (req, res) => {
-    // TODO: fill me in
+    res.json({space_animals: getAnimals(req.query.cowboy_name as string, spaceDatabase) as spaceAnimalCondensed[]})
 })
+
+// for logging errors
+app.use(morgan('dev'));
 
 app.listen(8080);
